7467e464136b12478585a1d4cab61c7bcdaf383b
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-08-31 18:41:09 -0300

    feat: search date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from tech_news.database import search_news
 
 
@@ -10,11 +12,16 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    try:
+        date_format = datetime.strptime(date, "%Y-%m-%d")
+        date_new_format = date_format.strftime("%d/%m/%Y")
+        search = search_news({"timestamp": {"$regex": date_new_format}})
+        list_news = [(news["title"], news["url"]) for news in search]
+        return list_news
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    ...
