c06c9e0d7b2662b99ffe18a8d617aa5e0c552968
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-08-31 19:42:18 -0300

    feat: top five category

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,18 @@
 # Requisito 10
+from collections import Counter
+
+from tech_news.database import find_news
+
+
 def top_5_categories():
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    categories = find_news()
+    categories_list = [category["category"] for category in categories]
+
+    categories_dict = Counter(categories_list)
+
+    top_5_categories = sorted(
+        categories_dict.keys(),
+        key=lambda category: (-categories_dict[category], category),
+    )[:5]
+
+    return top_5_categories
